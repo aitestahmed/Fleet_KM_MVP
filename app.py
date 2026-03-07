ca186deee948f3daa0bb7cd1b4fa81ca2ac7c6fa
aitestahmed <aitestahmed@gmail.com>
2026-03-07 22:13:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -375,22 +375,21 @@ if st.button("Generate AI Insight"):
 
 with st.expander("📊 AI Executive Report"):
 
-    st.markdown(
-        f"""
-        <div style="
-            background-color:#f9fafb;
-            padding:25px;
-            border-radius:10px;
-            border:1px solid #e5e7eb;
-            line-height:1.8;
-            font-size:16px;
-        ">
-        {report_html}
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-
+ st.markdown(
+    f"""
+    <div style="
+        background-color:#f9fafb;
+        padding:25px;
+        border-radius:10px;
+        border:1px solid #e5e7eb;
+        line-height:1.8;
+        font-size:16px;
+    ">
+    {report_html}
+    </div>
+    """,
+    unsafe_allow_html=True
+)
     # زر تحميل التقرير
     st.download_button(
         label="⬇ Download Fleet Report",
